91c302be94146138928edfcc9110ac7e6a61891d
zntly <83675710+zntly@users.noreply.github.com>
2022-08-17 21:58:14 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -148,10 +148,10 @@ while true do
         if Raised.Value > 999 then
             text = string.format("%.1fk", text / 10 ^ 3)
             --Booth text when 1000+ robux raised
-            boothText = tostring('<stroke color="#ffffff" thickness="2"><font color="#006400" face="PermanentMarker">Goal: ' .. text .. "</font></stroke>")
+            boothText = tostring([[<stroke color="#ffffff" thickness="2"><font color="#006400" face="PermanentMarker">Don't have any money after buying Korblox, pls help! \nGoal: ]] .. text .. "</font></stroke>")
         else
             --Booth text when under 1000 robux raised
-            boothText = tostring('<stroke color="#ffffff" thickness="2"><font color="#006400" face="PermanentMarker">Goal: ' .. Raised.value .. " / " .. text .. "</font></stroke>")
+            boothText = tostring([[<stroke color="#ffffff" thickness="2"><font color="#006400" face="PermanentMarker">Don't have any money after buying Korblox, pls help! \nGoal: ]] .. Raised.value .. " / " .. text .. "</font></stroke>")
         end
         --Updates the booth text
         require(game.ReplicatedStorage.Remotes).Event("SetBoothText"):FireServer(boothText, "booth")
